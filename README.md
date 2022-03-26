15d7c9b0bb7132887fadcf7d94805b27ea9e5bf7
Craig Leslie <cleslie25@gmail.com>
2022-03-26 00:26:04 -0500

    Added functionality and links in TOC

---
+++ README.md
@@ -0,0 +1,35 @@
+# kjhfakljhfd
+
+## Description
+
+a;khfak;djhf
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage Information](#usage)
+3. [Contribution Guidelines](#contribution)
+4. [Test Instructions](#test)
+5. [Questions](#questions)
+
+## Installation <a name="installation"></a>
+
+ajdfakjhdf
+
+## Usage Information <a name="usage"></a>
+
+a;jdhfakjl
+
+## Contribution Guidelines <a name="contribution"></a>
+
+lkjadflkja
+
+## Test Instructions <a name="test"></a>
+
+kjdfskljds
+
+## Questions <a name="questions"></a>
+
+[GitHub](https://github.com/3roses)<br>
+[Email](mailto:cleslie25@gmail.com)
+
--- index.js
+++ index.js
@@ -7,40 +7,85 @@ const userQuestions = () => {
     var response = inquirer.prompt([
         {
             type: 'input',
-            name: 'username',
-            message: 'what is your name?'
+            name: 'title',
+            message: 'What is the title of your project'
         },
         {
             type: 'input',
-            name: 'user_location',
-            message: 'what is your location?'
+            name: 'description',
+            message: 'Type a description of your project'
         },
-        // {
-        //     type: 'editor',
-        //     name: 'userBio',
-        //     message: 'press enter to input your bio info',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'user_linkd',
-        //     message: 'enter your user LinkedIn url'
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'user_git',
-        //     message: 'enter your user git hub url'
-        // }
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions for this project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the prime uses for this project?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are the contribution guidelines?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'How should a user test this application?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your gitHub url'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email'
+        },
     ])
     return response;
  }
 
- const generateReadme = ({username}) =>
-    `#Description
+ const generateReadme = ({title, description, installation, usage, contribution, test, github, email}) =>
+`# ${title}
 
-    ${}
+## Description
 
-    #Table of Contents
-    `
+${description}
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage Information](#usage)
+3. [Contribution Guidelines](#contribution)
+4. [Test Instructions](#test)
+5. [Questions](#questions)
+
+## Installation <a name="installation"></a>
+
+${installation}
+
+## Usage Information <a name="usage"></a>
+
+${usage}
+
+## Contribution Guidelines <a name="contribution"></a>
+
+${contribution}
+
+## Test Instructions <a name="test"></a>
+
+${test}
+
+## Questions <a name="questions"></a>
+
+[GitHub](${github})<br>
+[Email](mailto:${email})
+
+`;
 
  const initReadme = () => {
     userQuestions()
